791047ea11a1ab736b7fc6fc03cbb45a346fecf9
Mike Schatz <mschatz@cshl.edu>
2014-03-04 14:24:09 -0500

    remove old volume

--- bin/cron_record_space.sh
+++ bin/cron_record_space.sh
@@ -5,7 +5,6 @@ outdir=/var/www/vhosts/yellowstone.cshl.edu/spacemonitor
 /root/bin/record_space.sh /seq/schatz $outdir/seq-schatz &
 /root/bin/record_space.sh /bluearc/data/schatz $outdir/bluearc-data-schatz &
 /root/bin/record_space.sh /bluearc/data/schatz2 $outdir/bluearc-data-schatz2 &
-/root/bin/record_space.sh /bluearc/datafc/schatz $outdir/bluearc-datafc-schatz &
 /root/bin/record_space.sh /bluearc/home/schatz $outdir/bluearc-home-schatz &
 /root/bin/record_space.sh /sonas-hs/schatz/hpc/data $outdir/sonas-hs-schatz &
 /root/bin/record_space.sh /home $outdir/yellowstone-home &
